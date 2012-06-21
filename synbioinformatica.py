00a8c53dcfea653e3e80e7efb97d5babd22e1337
Nima Emami <timanoed154@yahoo.com>
2012-06-21 01:52:18 -0700

    ligate fxn

--- synbioinformatica.py
+++ synbioinformatica.py
@@ -873,10 +873,10 @@ def Ligate(inputDNAs):
 	for fragment in inputDNAs:
 		(TL,TR,BL,BR) = SetFlags(fragment)
 		if fragment.DNAclass != 'digest':
-			print 'WARNING: Invalid input molecule removed -- ligation input DNA objects must be of class \'digest\'.'
+			print 'WARNING: For ligation reaction, invalid input molecule removed -- ligation input DNA objects must be of class \'digest\'.'
 			inputDNAs.remove(fragment)
 		elif TL+TR+BL+BR == 1 or TL+TR+BL+BR == 0:
-			print 'WARNING: Invalid input molecule removed -- blunt end ligation inputs disallowed'
+			print 'WARNING: For ligation reaction, invalid input molecule removed -- blunt end ligation inputs disallowed'
 			inputDNAs.remove(fragment)
 		if fragment.topLeftOverhang.sequence != '':
 			if fragment.topLeftOverhang.sequence.lower() == Complement(fragment.bottomRightOverhang.sequence.lower()):
